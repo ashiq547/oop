15e4ae2cdbdb4ab6fb5a3f554f299b5a53ef3dae
muaaz121 <muaazkhan7278364@gmail.com>
2024-07-15 21:55:31 +0600

    updatedloginscene
    just updated the user's  login scene!

--- src/main/java/com/muaaz/tutorial_2/fishhatcharyfarm/HelloController.java
+++ src/main/java/com/muaaz/tutorial_2/fishhatcharyfarm/HelloController.java
@@ -1,14 +1,31 @@
 package com.muaaz.tutorial_2.fishhatcharyfarm;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usernameinput;
+    @FXML
+    private TextField passwordinput;
+
+ //   @FXML
+ //   protected void onHelloButtonClick() {
+      //  welcomeText.setText("Welcome to JavaFX Application!");
+    //}
+
+    @FXML
+    public void signinOnClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void changepasswordOnclick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void registeruserOnclick(ActionEvent actionEvent) {
     }
 }
